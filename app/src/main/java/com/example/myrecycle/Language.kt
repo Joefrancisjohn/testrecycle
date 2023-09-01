5a883b017a8d7ded714988b22d7aa3bf3c2da7cc
JoeFJ <jjo1989@safeway.com>
2023-09-01 23:51:05 +0530

    initial commit

---
+++ app/src/main/java/com/example/myrecycle/Language.kt
@@ -0,0 +1,7 @@
+package com.example.myrecycle
+
+// this is the Language model class
+class Language(
+    val name : String ="",
+    val exp : String =""
+)
---
+++ app/src/main/java/com/example/myrecycle/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.example.myrecycle
+
+import com.google.android.material.snackbar.Snackbar
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.myrecycle.databinding.ActivityMainBinding
+
+/*
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAnchorView(R.id.fab)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}*/
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity() {
+
+    // view binding for the activity
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    // create reference to the adapter and the list
+    // in the list pass the model of Language
+    private lateinit var rvAdapter: RvAdapter
+    private lateinit var languageList : List<Language>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // load data to language list
+        loadLanguage()
+
+        // create layoutManager
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+
+        // pass it to rvLists layoutManager
+        binding.rvList.setLayoutManager(layoutManager)
+
+        // initialize the adapter,
+        // and pass the required argument
+        rvAdapter = RvAdapter(languageList)
+
+        // attach adapter to the recycler view
+        binding.rvList.adapter = rvAdapter
+    }
+
+    // add items to the list manually in our case
+    private fun loadLanguage() {
+        languageList = listOf(
+            Language("Java" , "Exp : 3 years"),
+            Language("Kotlin" , "Exp : 2 years"),
+            Language("Python" , "Exp : 4 years"),
+            Language("JavaScript" , "Exp : 6 years"),
+            Language("PHP" , "Exp : 1 years"),
+            Language("CPP" , "Exp : 8 years"),
+        )
+    }
+
+    // on destroy of view make
+    // the binding reference to null
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
